603b88e457c712f50b71c85f941024deb2c45c93
Thiruppathitpra <thiruppathitpra@gmail.com>
2026-09-03 10:15:25 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const router = express.Router();
+
 const jwt = require("jsonwebtoken");
 
-const router = express.Router();
 
 const { User } = require("../models/models");
 
